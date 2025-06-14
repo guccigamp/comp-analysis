7faca8a1f483a25b7b91c37109c31b59f8d76fc0
Aagam Shah <shahaagam04@gmail.com>
2025-06-14 15:29:09 -0500

    feat: add server Docker configuration and update client environment variables
    - Introduced docker-compose-dev.yml for server development setup.
    - Added env_file configuration to client docker-compose for environment variable management.
    - Updated API_URL in api.js to use VITE_API for improved API integration.

--- apps/client/src/lib/api.js
+++ apps/client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = `${import.meta.env.API_URL}`;
-
+const API_URL = import.meta.env.VITE_API;
+console.log(API_URL);
 const api = axios.create({
     baseURL: API_URL,
     headers: {
